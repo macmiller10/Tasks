1127cf1ac65d9e19792669e61b0db94c91738238
macmiller10 <macmiller0410@gmail.com>
2022-01-13 19:55:17 -0500

    <mackenzie miller> Task 01 upload b

--- Task_01/Task_01.r
+++ Task_01/Task_01.r
@@ -1,4 +1,21 @@
 # pw: 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
 #install.packages ("swirl")
 library (swirl)
-
+swirl ()
+Macmiller10
+5+7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+my_sqrt
+my_div <- z / my_sqrt
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
